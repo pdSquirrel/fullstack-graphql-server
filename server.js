542af6a47326827aa32e86468d301dee9a8e0934
Roger Nordqvist <progdev.squirrel@gmail.com>
2018-05-21 23:13:29 +0200

    added mongoose and setup mongodb connection

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import {graphqlExpress, graphiqlExpress} from 'apollo-server-express'
+import express from 'express';
+import bodyParser from 'body-parser';
+import {graphqlExpress, graphiqlExpress} from 'apollo-server-express';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
 import schema from './graphql/schema';
 
-const server = express()
+const server = express();
+server.use(cors());
+
+mongoose.connect('mongodb://localhost/graphqlserver');
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+})
 
 server.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
 server.use('/graphql', bodyParser.json(), graphqlExpress({schema: schema}));
 
 server.listen(4000, () => {
-  console.log('Server started on port 4000')
+  console.log('Server started on port 4000');
 });
