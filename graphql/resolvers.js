aa3a690c223e0f42b4fbe2b0f8544a7e1bf448a5
Roger Nordqvist <progdev.squirrel@gmail.com>
2018-05-23 22:37:56 +0200

    refactor to use mongoose as backend storage

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,3 +1,7 @@
+import mongoose from 'mongoose';
+import courseModel from '../models/course';
+
+/*
 const coursesData = [
   {
     id: '1',
@@ -25,37 +29,68 @@ const coursesData = [
     voteCount: 0
   }
 ];
+*/
 
 const resolvers = {
   Query: {
     allCourses: (root, {searchTerm}) => {
-      return coursesData
+      /* return coursesData */
+      if (searchTerm !== '') {
+        return courseModel
+          .find({
+          $text: {
+            $search: searchTerm
+          }
+        })
+          .sort({voteCount: 'desc'});
+      } else {
+        return courseModel
+          .find()
+          .sort({voteCount: 'desc'});
+      }
+
     },
     course: (root, {id}) => {
-      return coursesData.filter(course => {
+      /* return coursesData.filter(course => {
         return course.id === id;
-      })[0];
-      /* return courseModel.findOne({id: id}) */
+      })[0]; */
+      return courseModel.findOne({id: id});
     }
   },
 
   Mutation: {
     upvote: (root, {id}) => {
-      const course = coursesData.filter(course => {
+      /* const course = coursesData.filter(course => {
         return course.id === id;
       })[0]
       course.voteCount++;
-      return course;
+      return course; */
+      return courseModel.findOneAndUpdate({
+        id: id
+      }, {
+        $inc: {
+          'voteCount': 1
+        }
+      }, {returnNewDocument: true});
     },
     downvote: (root, {id}) => {
-      const course = coursesData.filter(course => {
+      /* const course = coursesData.filter(course => {
         return course.id === id;
-      })[0]/* answer will be returned in an array, thats why we pick it of with [0] */
+      })[0]// answer will be returned in an array, thats why we pick it of with [0]
       course.voteCount--;
-      return course;
+      return course; */
+      return courseModel.findOneAndUpdate({
+        id: id
+      }, {
+        $inc: {
+          'voteCount': -1
+        }
+      }, {returnNewDocument: true});
     },
     addCourse: (root, {title, author, description, topic, url}) => {
-      return null;
+      /* return null; */
+      const course = new courseModel({title: title, author: author, description: description, topic: topic, url: url});
+      return course.save();
     }
   }
 }
